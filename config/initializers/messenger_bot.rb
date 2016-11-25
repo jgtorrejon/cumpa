6b98abf68f3014ec9a1f5f506cbcb94bdd9de019
darlyn <bpdarlyn@gmail.com>
2016-11-25 15:46:00 -0400

    CB-6 :(

---
+++ config/initializers/messenger_bot.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# ApplicationController.renderer.defaults.merge!(
+#   http_host: 'example.org',
+#   https: false
+# )
+
+Messenger::Bot.config do |config|
+  config.access_token = 'EAAKXfG78pMcBAJGzoWgXNm0MfV1NncAl4DFDzF587vlVoYzkbp0kuZBJrbHpyUgLrkcQmZAxZCnWY9YwCt8d82UVoHTFoYZCK1QdAiGqRP68JIQaEqWXj1Eor3gDpireXrv7qAHnmOZBTROuiOEqXOZBcnrkDePTuqZAqTs2qzaRAZDZD'
+      config.validation_token = 'hackaton_bnb'
+      config.secret_token = 'eb467478c41cd3b2fd77f0499749ef96'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  get 'home/api_ai'
   root "home#index"
+  mount Messenger::Bot::Space => "/webhook"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
