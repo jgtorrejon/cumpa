c9eb8c58e66d1eafaf35fd99b6cc1654c5796714
darlyn <bpdarlyn@gmail.com>
2016-11-26 08:18:23 -0400

    :) CB-6

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,11 +1,11 @@
 class MessengerController < Messenger::MessengerController
   def webhook
     # text=params[:entry].first["messaging"].first["message"][:text]
+    params[:entry].first[:messaging].first[:message].delete :quick_reply
     fb_params.entries.each do |entry|
       process_response(entry)
     end
     render nothing: true, status: 200
-
   end
 
   def process_response(entry)
@@ -38,7 +38,6 @@ class MessengerController < Messenger::MessengerController
       model_response=send_to_api_ai(message.text)
       command_response= model_response[:result][:action] # accion
       message_response= model_response[:result][:fulfillment][:speech] #respuesta
-      puts message_response
       clasify_messagin(command_response, message_response)
     end
 
@@ -65,7 +64,6 @@ class MessengerController < Messenger::MessengerController
   end
 
   def clasify_messagin(command, response_text)
-    puts [command,response_text]
     case command
       when "FAQS_GET_CARD"
         request_base(Messenger::Elements::Text.new(text: response_text))
@@ -77,6 +75,32 @@ class MessengerController < Messenger::MessengerController
                 Messenger::Elements::Button.new(type: 'postback', title: 'Natural', value: 'FAQS_OPEN_ACCOUNT_NATURAL')
             ]
         ))
+      when "EXCHANGE_RATE"
+        exchange = ExchangeRate.first
+        request_base(Messenger::Elements::Text.new(text: response_text))
+        response = "Dolar Venta: " + exchange.buy.to_s
+        response += "\u000A Dolar Compra: " + exchange.sell.to_s
+        response += "\u000A UFV: " + exchange.ufv.to_s
+        request_base(Messenger::Elements::Text.new(text: response.encode('utf-8')))
+
+      when "FAQS_BNBNET_ACCESS"
+        request_base(Messenger::Elements::Text.new(text: response_text))
+
+        bubble1 = bubble_base('Ingrese la siguiente pagina', 'Vaya a la parte superior derecha',
+                              request.base_url.to_s+'/assets/primero_paso.png')
+        bubble2 = bubble_base('Click en la imagen BNB NET', 'imagen ubicada al centro',
+                              request.base_url.to_s+'/assets/segundo_paso.png')
+        bubble3 = bubble_base('Ingrese su credencial otorgado por el banco', 'No olvidar el recaptcha!!.',
+                              request.base_url.to_s+'/assets/tercer_paso.png')
+        request_base(Messenger::Templates::Generic.new(elements:[bubble1,bubble2,bubble3]))
+        when "FAQS_PAYMENTS_SERVICES"
+        request_base(Messenger::Elements::Text.new(text: response_text))
+
+        bubble1 = bubble_base_without_image('Telefonia', 'Viva, Tigo, Entel')
+        bubble2 = bubble_base_without_image('Serv. Basicos', 'Cre, Saguapac')
+        bubble3 = bubble_base_without_image('Seguros', 'Boliviana y Vitalicia')
+        request_base(Messenger::Templates::Generic.new(elements:[bubble1,bubble2,bubble3]))
+
     end
   end
   def clasify_postback(command)
@@ -85,6 +109,41 @@ class MessengerController < Messenger::MessengerController
         request_base(Messenger::Elements::Text.new(text: "Solo Necesitas llevar tu carnet de identidad."))
       when "FAQS_OPEN_ACCOUNT_JURIDICO"
         request_base(Messenger::Elements::Text.new(text: "Necesitas Documento legal de la empresa y representantes."))
+      when "FAQS"
+        body_request={
+            :recipient => {:id =>@user_id},
+            :message=> {
+                :text => "Seleccione una pregunta",
+                "quick_replies":[
+                    {
+                        "content_type"=>"text",
+                        "title"=>"Como entro a bnb.net?",
+                        "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED",
+                    },
+                    {
+                        "content_type"=>"text",
+                        "title"=>"Perdi mi tarjeta que hago ?",
+                        "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN",
+                    },
+                    {
+                        "content_type"=>"text",
+                        "title"=>"Quiero aperturar una cuenta",
+                        "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN",
+                    },
+                    {
+                        "content_type"=>"text",
+                        "title"=>"Que servicios puedo pagar en el banco ?",
+                        "payload"=>"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN",
+                    }
+                ]
+            }
+        }
+        header_request={
+            "Content-Type"=>"application/json"
+        }
+        url="https://graph.facebook.com/v2.6/me/messages?access_token="+Messenger.config.page_access_token
+        create_request(url,body_request,header_request)
+
     end
   end
 
@@ -96,4 +155,32 @@ class MessengerController < Messenger::MessengerController
         )
     )
   end
+
+  def bubble_base(title, subtitle, url_image)
+    bubble = Messenger::Elements::Bubble.new(
+        title: title,
+        subtitle: subtitle,
+        image_url: url_image ,
+    )
+  end
+  def bubble_base_without_image(title, subtitle)
+    bubble = Messenger::Elements::Bubble.new(
+        title: title,
+        subtitle: subtitle
+    )
+  end
+
+  def create_request(url,hash_body,headers)
+    HTTParty.post(url,
+                  :body => hash_body.to_json,
+                  :headers => headers )
+  end
+
+  def f x
+    x.is_a?(Hash) ? x.inject({}) do |m, (k, v)|
+      m[k] = f v unless k == 'quick_reply'
+      m
+    end : x
+  end
+
 end
