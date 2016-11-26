53fa89c41279f14dc2551e4f7364db129bedf144
darlyn <bpdarlyn@gmail.com>
2016-11-25 23:27:32 -0400

    fixed

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -3,29 +3,32 @@ class MessengerController < Messenger::MessengerController
     #logic here
     # text=params[:entry].first["messaging"].first["message"][:text]
     fb_params.entries.each do |entry|
-      entry.messagings.each do |messaging|
-        if messaging.callback.message?
-          # profile= Messenger::Client.get_user_profile(messaging.sender_id)
-          # Messenger::Client.send(
-          #     Messenger::Request.new(
-          #         Messenger::Elements::Text.new(text: "#{messaging.callback.text}"),
-          #         messaging.sender_id
-          #     )
-          # )
-          client = ApiAiRuby::Client.new(:client_access_token => '5d8413c5badb49b4969a6e70a9d65edb')
-          response = client.text_request messaging.callback.text
-          response_api_ai_text= response[:result][:fulfillment][:speech]
-          puts response_api_ai_text
-          Messenger::Client.send(
-              Messenger::Request.new(
-                  Messenger::Elements::Text.new(text: "#{response_api_ai_text}"),
-                  messaging.sender_id
-              )
-          )
-          # puts messaging.callback.text
-        end
-      end
+      process_response(entry)
     end
     render nothing: true, status: 200
+
+  end
+
+  def process_response(entry)
+    entry.messagings.each do |messaging|
+      if messaging.callback.message?
+        puts "arrive message"
+      elsif messaging.callback.delivery?
+        puts "arrive delivery"
+      elsif messaging.callback.postback?
+        puts "arrive postback"
+      elsif messaging.callback.optin?
+        puts "arrive option"
+      elsif messaging.callback.account_linking?
+        puts "arrive acount_link"
+      end
+      # Messenger::Client.send(
+      #     Messenger::Request.new(
+      #         Messenger::Elements::Text.new(text: "Echo: #{messaging.callback.text}"),
+      #         messaging.sender_id
+      #     )
+      # )
+
+    end
   end
 end
---
+++ lib/modules/apiai.rb
@@ -0,0 +1,29 @@
+module Apiai
+  # def parse_model(value_hash, auto_send=true)
+  #
+  #
+  #   model_api_ai= Struct.new(response)
+  #   if aut_send
+  #     send_response(model_api_ai)
+  #   end
+  #   model_api_ai
+  # end
+  #
+  # def send_response(model=nil)
+  #   client = ApiAiRuby::Client.new(:client_access_token => self.ACCESS_TOKEN)
+  #   response = client.text_request messaging.callback.text
+  #   # response_api_ai_text= response[:result][:fulfillment][:speech]
+  #   Messenger::Client.send(
+  #       Messenger::Request.new(
+  #           Messenger::Elements::Text.new(text: "#{model.try(:result).try(:fulfillment).try(:speech)}"),
+  #           messaging.sender_id
+  #       )
+  #   )
+  # end
+  #
+  # def ACCESS_TOKEN
+  #   '5d8413c5badb49b4969a6e70a9d65edb' #todo new struct by joel
+  # end
+
+
+end
