594955859e0c72978523c5088772322b9d56126e
Joel Torrejon <joel.torrejon.mendez@gmail.com>
2017-11-10 00:31:49 -0400

    set new config keys messenger

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -7,6 +7,6 @@
 
 # YOUR_APP/config/initializers/messenger.rb
 Messenger.configure do |config|
-  config.verify_token      = 'cumpterextrasexpress' #will be used in webhook verifiction
-  config.page_access_token = 'EAAbZA5E3Ec5MBAMPlL7vM1x0ZAt0b4bhD7FrejwtCZATzRGVksJ8yImL8o8G7PWonOAw5UMUuPYk9fHpOuiR4fry8Twz5Oi0jyUldK68PHrH71Y3ByAd0yezLI7fecCOqhQdrAaeFxwhwnWXIXX5iesCcZACyZCZCz1mex2sN3qIfEHQmEr49U'
+  config.verify_token      = 'computer-extras-express' #will be used in webhook verifiction
+  config.page_access_token = 'EAAbZA5E3Ec5MBAO5O3WFp8xXDipgC4ebjmsdZA8QmGsHxPm2qwvAZBg3ZABTqra4uJiYPf3iGnXU0v2ch24VQJVbmpSD6tqZAsfm9wTDsSPZANkiYfPjeakaULZBZBTlWkVGS6ixUDFVVooS8Gbu0znTZAEMmXZAQhgCRLwkpLtgWhtdjBvMVv82lS'
 end
